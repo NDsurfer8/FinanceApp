7256d5b5f62a41947da805787740574b9a5349c3
Noah Duran <noahduran08@gmail.com>
2025-09-02 15:25:43 -1000

    update

--- money-pilot/src/services/plaid.ts
+++ money-pilot/src/services/plaid.ts
@@ -13,11 +13,11 @@ import {
   LinkLogLevel,
   LinkIOSPresentationStyle,
 } from "react-native-plaid-link-sdk";
-// used to just have transactions
+
 // Plaid configuration - you'll need to get these from your Plaid dashboard
 export const PLAID_CONFIG = {
   clientName: "VectorFi",
-  products: ["transactions", "liabilities", "investments"],
+  products: ["transactions"],
   countryCodes: ["US"],
   language: "en",
 };
